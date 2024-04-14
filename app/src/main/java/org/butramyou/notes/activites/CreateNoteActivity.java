9e9ea7b84427be642472ee03b5c3cd1407ddf3fd
Dzmitry Butramyou <dzmitry.butramyou@onpier.de>
2024-04-14 12:07:28 +0200

    Create & Save Note

--- app/src/main/java/org/butramyou/notes/activites/CreateNoteActivity.java
+++ app/src/main/java/org/butramyou/notes/activites/CreateNoteActivity.java
@@ -1,13 +1,27 @@
 package org.butramyou.notes.activites;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.butramyou.notes.R;
+import org.butramyou.notes.database.NotesDatabase;
+import org.butramyou.notes.entities.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CreateNoteActivity extends AppCompatActivity {
+    private EditText inputNoteTitle, inputNoteSubtitle, inputNoteText;
+    private TextView textDateTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +30,46 @@ public class CreateNoteActivity extends AppCompatActivity {
 
         ImageView imageBack = findViewById(R.id.imageBack);
         imageBack.setOnClickListener(v -> getOnBackPressedDispatcher().onBackPressed());
+
+        inputNoteTitle = findViewById(R.id.inputNoteTitle);
+        inputNoteSubtitle = findViewById(R.id.inputNoteSubtitle);
+        inputNoteText = findViewById(R.id.inputNoteText);
+
+        textDateTime = findViewById(R.id.textDateTime);
+        textDateTime.setText(new SimpleDateFormat(
+                "EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault()).format(new Date().getTime())
+        );
+
+        ImageView imageSave = findViewById(R.id.imageSave);
+        imageSave.setOnClickListener(v -> saveNote());
+    }
+
+    private void saveNote() {
+        final String noteTitle = inputNoteTitle.getText().toString().trim();
+        final String noteSubtitle = inputNoteSubtitle.getText().toString().trim();
+        final String noteText = inputNoteText.getText().toString().trim();
+        final String dateTimeStr = textDateTime.getText().toString().trim();
+
+        if (noteTitle.isEmpty()) {
+            Toast.makeText(this, "Note title can't be empty!", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (noteSubtitle.isEmpty() && noteText.isEmpty()) {
+            Toast.makeText(this, "Note can't be empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        final Note note = new Note();
+        note.setTitle(noteTitle);
+        note.setSubtitle(noteSubtitle);
+        note.setNoteText(noteText);
+        note.setDateTime(dateTimeStr);
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(() -> {
+            NotesDatabase.getNotesDatabase(getApplicationContext()).noteDao().insertNote(note);
+            Intent intent = new Intent();
+            setResult(RESULT_OK, intent);
+            finish();
+        });
     }
 }
--- app/src/main/java/org/butramyou/notes/activites/MainActivity.java
+++ app/src/main/java/org/butramyou/notes/activites/MainActivity.java
@@ -2,13 +2,21 @@ package org.butramyou.notes.activites;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.butramyou.notes.R;
+import org.butramyou.notes.database.NotesDatabase;
+import org.butramyou.notes.entities.Note;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     public static final int REQUEST_CODE_ADD_NOTE = 1;
 
@@ -21,5 +29,17 @@ public class MainActivity extends AppCompatActivity {
         imageAddNoteMain.setOnClickListener(v -> startActivityForResult(new Intent(
                 getApplicationContext(), CreateNoteActivity.class), REQUEST_CODE_ADD_NOTE)
         );
+
+        getNotes();
+    }
+
+
+    private void getNotes() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(() -> {
+            List<Note> notes = NotesDatabase.getNotesDatabase(getApplicationContext())
+                    .noteDao().getAllNotes();
+            Log.d(TAG, "onPostExecute: " + notes.toString());
+        });
     }
 }
